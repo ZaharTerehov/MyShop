fdadd2aa5be653555ceb8b104193c822289b3c6e
z.terehov <zaharkuli67@gmail.com>
2022-12-08 23:32:30 +0300

    added interfaces

---
+++ MyShop/MyShop/Interfaces/ICatalogItemViewModelService.cs
@@ -0,0 +1,12 @@
+﻿using MyShop.Models.ViewModel;
+
+namespace MyShop.Interfaces
+{
+    public interface ICatalogItemViewModelService
+    {
+        public void UpdateCatalogItem(CatalogItemViewModel viewModel)
+        {
+
+        }
+    }
+}
---
+++ MyShop/MyShop/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace MyShop.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        public T? GetById(Guid id);
+
+        public void Update(T entity);
+
+        public List<T> GetAll();
+    }
+}
